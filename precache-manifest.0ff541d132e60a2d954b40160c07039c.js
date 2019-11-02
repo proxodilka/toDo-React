2360ea17df993063b47a81cdaa9b50ca3cf05fa0
proxodilka <zeeron209@gmail.com>
2019-11-02 19:05:07 +0400

    Updates

---
+++ precache-manifest.0ff541d132e60a2d954b40160c07039c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b45e585c36fea9ea0e551f403ac654",
+    "url": "/toDo-React/index.html"
+  },
+  {
+    "revision": "7190d303d7bb4cdb90f0",
+    "url": "/toDo-React/static/css/main.2ce80367.chunk.css"
+  },
+  {
+    "revision": "7b8227d8e57c8d980a08",
+    "url": "/toDo-React/static/js/2.28299dee.chunk.js"
+  },
+  {
+    "revision": "7190d303d7bb4cdb90f0",
+    "url": "/toDo-React/static/js/main.271e94af.chunk.js"
+  },
+  {
+    "revision": "aa8647ab5b3dcdbad29b",
+    "url": "/toDo-React/static/js/runtime~main.5d1d3606.js"
+  }
+]);
